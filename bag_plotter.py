bfacb66906f1f70941496934468bc865abce0746
fred <fred.vatnsdal@gmail.com>
2025-07-22 13:18:04 -0400

    move catpuccin colors

--- bag_plotter.py
+++ bag_plotter.py
@@ -185,18 +185,6 @@ def plot_bag(bag_dict: dict,
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
 
-    catpuccin_colors = [
-        'rgba(239, 159, 118, 1.0)',  # Peach
-        'rgba(166, 209, 137, 1.0)',  # Green
-        'rgba(202, 158, 230, 1.0)',  # Mauve
-        'rgba(133, 193, 220, 1.0)',  # Sapphire
-        'rgba(231, 130, 132, 1.0)',  # Red
-        'rgba(129, 200, 190, 1.0)',  # Teal
-        'rgba(242, 213, 207, 1.0)',  # Rosewater
-        'rgba(229, 200, 144, 1.0)',  # Yellow
-        'rgba(108, 111, 133, 1.0)',  # subtext0
-    ]
-
     colors = seaborn_colors(catpuccin_colors)
     set_theme()
 
--- colors.py
+++ colors.py
@@ -3,3 +3,15 @@ GREEN = "\033[32m"
 YELLOW = "\033[33m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
+
+catpuccin_colors = [
+    'rgba(239, 159, 118, 1.0)',  # Peach
+    'rgba(166, 209, 137, 1.0)',  # Green
+    'rgba(202, 158, 230, 1.0)',  # Mauve
+    'rgba(133, 193, 220, 1.0)',  # Sapphire
+    'rgba(231, 130, 132, 1.0)',  # Red
+    'rgba(129, 200, 190, 1.0)',  # Teal
+    'rgba(242, 213, 207, 1.0)',  # Rosewater
+    'rgba(229, 200, 144, 1.0)',  # Yellow
+    'rgba(108, 111, 133, 1.0)',  # subtext0
+]
